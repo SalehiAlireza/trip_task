86656c69aa6ce52a11debbf1682ab75aca75e24e
AlirezaSalehi <smartapply.it8@gmail.com>
2024-06-08 14:06:29 +0330

    add migrations

---
+++ database/migrations/2024_06_08_102616_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->charset('utf8');
+            $table->unsignedBigInteger('driver_id')->nullable();
+            $table->unsignedBigInteger('truck_id')->nullable();
+            $table->unsignedBigInteger('task_id')->nullable();
+
+            # create foreign keys
+            $table->foreign('driver_id')->on('drivers')->references('id')->onDelete('set null');
+            $table->foreign('truck_id')->on('trucks')->references('id')->onDelete('set null');
+            $table->foreign('task_id')->on('tasks')->references('id')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
