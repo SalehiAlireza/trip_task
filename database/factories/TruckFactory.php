4c52abd90b49178f6bbfb5dada613a7b31f9e916
AlirezaSalehi <smartapply.it8@gmail.com>
2024-06-08 14:43:01 +0330

    serving seeder and factory

---
+++ database/factories/TruckFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Truck>
+ */
+class TruckFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $modelList = ['Toyota Tundra','Nissan Frontier','Toyota Tacoma','Ford F-Series','GMC Canyon'];
+        return [
+            'truck_model' => $modelList[random_int(0,4)],
+            'truck_code'  => Str::random('4').random_int(1000,9999)
+        ];
+    }
+}
