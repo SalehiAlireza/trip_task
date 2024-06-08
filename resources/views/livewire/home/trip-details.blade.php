59235db8a9cbb8cb9be86c54a07b62bb25784dde
AlirezaSalehi <smartapply.it8@gmail.com>
2024-06-08 15:18:48 +0330

    complete design trips details

--- resources/views/livewire/home/trip-details.blade.php
+++ resources/views/livewire/home/trip-details.blade.php
@@ -7,22 +7,48 @@
                     <input type="text" class="form-control" placeholder="Send message to...">
                     <a class="btn btn-danger close-new-message" href="#"><i class="fa fa-times"></i></a>
                 </div>
-                <div class="chat-footer new-message-textarea">
-                    <textarea class="send-message-text"></textarea>
-                    <label class="upload-file">
-                        <input type="file" required>
-                        <i class="fa fa-paperclip"></i>
-                    </label>
-                    <button type="button" class="send-message-button btn-info"><i class="fa fa-send"></i>
-                    </button>
-                </div>
             </div>
         </div>
         <div class="message-chat">
             <div class="chat-body">
+                <div>
+                    <button class="btn btn-info">Trip #1</button>
+                </div>
+                <hr>
+                <div>
+                    <h4>Show trip details : </h4>
+                    <table class="table bg-info table-bordered table-striped">
+                        <thead>
+                        <tr>
 
+                            <th scope="col">Driver</th>
+                            <th scope="col">Truck</th>
+                            <th scope="col">Truck Code</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+
+                    <table class="table bg-info table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="row">Tasks: </th>
+                                <td >
+                                    <span class="badge badge-info">asdad</span>
+                                </td>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
-
         </div>
     </div>
 </div>
