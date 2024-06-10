6514931a35d402ff7e68b6acdd1941ed3e97944e
AlirezaSalehi <smartapply.it8@gmail.com>
2024-06-10 15:06:37 +0330

    update tasks

--- resources/views/livewire/home/task-list.blade.php
+++ resources/views/livewire/home/task-list.blade.php
@@ -5,11 +5,10 @@
             <h4 style="padding: 10px 10px;">Task Lists : </h4>
             <ul class="list-unstyled contacts">
                 @foreach($tasks as $taskItem)
-                    <li data-toggle="tab" data-target="#inbox-message-1" class="active">
+                    <li data-toggle="tab"  data-target="#inbox-message-1" class="active">
                         <div>
                             <p class="text-center">
-                                <i class="fa fa-trash-o" wire:click="deleteTask({{ $taskItem->id  }})" style="cursor: pointer;color: red !important;"></i>
-                            <div class="message-count"> 1</div>
+                                <i class="fa fa-trash-o" wire:key="task-{{ $taskItem->id  }}" wire:click="deleteTask({{ $taskItem->id  }})" style="cursor: pointer;color: red !important;"></i>
                             </p>
                         </div>
                         <div class="vcentered info-combo">
